43169df5169ae529519f74344557b91ea6d7f078
Burcu <burcuayz23@gmail.com>
2020-10-24 19:50:19 +0300

    bir satır aşağı indi

--- Coopmas/Models/talep.cs
+++ Coopmas/Models/talep.cs
@@ -13,6 +13,7 @@ namespace Coopmas.Models
         public string soru1 { get; set; }
         public DateTime soru2 { get; set; }
         public string soru3 { get; set; }
+
         public int soru4 { get; set; }
         public string email { get; set; }
         public int telephone { get; set; }
